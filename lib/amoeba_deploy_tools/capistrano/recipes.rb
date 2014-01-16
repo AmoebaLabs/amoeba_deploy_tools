99b89b7765dfa852c3d8e0e735ac188a3b8a921d
Daniel Jabbour <daniel@amoe.ba>
2014-01-16 07:13:00 -0800

    Adding capistrano recipes

--- lib/amoeba_deploy_tools/capistrano/recipes.rb
+++ lib/amoeba_deploy_tools/capistrano/recipes.rb
@@ -11,4 +11,31 @@ Capistrano::Configuration.instance(:must_exist).load do
           ].join(' && ')
     end
   end
+
+  namespace :deploy do
+    desc "create the database (and load seed data)"
+    task :db_setup do
+      run "cd #{current_path}; bundle exec rake db:setup RAILS_ENV=#{rails_env}"
+    end
+
+    desc <<-DESC
+    Deploys and starts a `cold' application. This is useful if you have not \
+    deployed your application before, or if your application is (for some \
+    other reason) not currently running. It will deploy the code, create and seed the DB, run any \
+    pending migrations, and then instead of invoking `deploy:restart', it will \
+    invoke `deploy:start' to fire up the application servers.
+    DESC
+    task :cold do
+      update
+      db_setup
+      migrate
+      start
+    end
+
+    desc "restart the application (trigger passenger restart)"
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+  end
+
 end
